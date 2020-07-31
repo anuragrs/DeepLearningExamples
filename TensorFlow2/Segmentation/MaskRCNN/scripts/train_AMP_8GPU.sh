39e194367db29b1151e86c69c2ef6f6b3069b715
Ubuntu <ubuntu@ip-192-168-90-208.ec2.internal>
2020-07-31 02:23:38 +0000

    More changes to single node

--- TensorFlow2/Segmentation/MaskRCNN/scripts/train_AMP_8GPU.sh
+++ TensorFlow2/Segmentation/MaskRCNN/scripts/train_AMP_8GPU.sh
@@ -14,29 +14,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-rm -rf /results
+rm -rf /shared/results
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-/opt/amazon/openmpi/bin/mpirun --allow-run-as-root --tag-output \
+/opt/amazon/openmpi/bin/mpirun --allow-run-as-root --tag-output --mca plm_rsh_no_tree_spawn 1 \
     -np 8 \
+    --mca btl_tcp_if_exclude lo,docker0 \
     -x NCCL_DEBUG=VERSION \
     -x LD_LIBRARY_PATH \
     -x PATH \
-    python ${BASEDIR}/../mask_rcnn_main.py \
+    --oversubscribe \
+    /shared/conda/bin/python ${BASEDIR}/../mask_rcnn_main.py \
         --mode="train_and_eval" \
-        --checkpoint="/model/resnet/resnet-nhwc-2018-02-07/model.ckpt-112603" \
+        --checkpoint="/shared/model/resnet/resnet-nhwc-2018-02-07/model.ckpt-112603" \
         --eval_samples=5000 \
         --init_learning_rate=0.04 \
         --learning_rate_steps="30000,40000" \
-        --model_dir="/results/" \
+        --model_dir="/shared/results/" \
         --num_steps_per_eval=3696 \
         --total_steps=1000 \
         --train_batch_size=4 \
         --eval_batch_size=8 \
-        --training_file_pattern="/data/train*.tfrecord" \
-        --validation_file_pattern="/data/val*.tfrecord" \
-        --val_json_file="/data/annotations/instances_val2017.json" \
+        --training_file_pattern="/shared/data/train*.tfrecord" \
+        --validation_file_pattern="/shared/data/val*.tfrecord" \
+        --val_json_file="/shared/data/annotations/instances_val2017.json" \
         --amp \
         --use_batched_nms \
         --xla \
